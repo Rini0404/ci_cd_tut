e5f10e71a3be92a3a2460d3b3bd27df3a7bc28a6
Rini <reneortega2468@gmail.com>
2024-02-01 18:26:27 -0500

    Initial fixed

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,13 @@
-require('events').EventEmitter.defaultMaxListeners = 20; 
 
-require("dotenv").config();
 const express = require("express");
-
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 
-const googleAuth = require("./api/routes/google_auth")
 
 app.get("/", (req: any, res: any) => {
   res.send("Hello World!");
 })
-
-app.use("/api/v1", googleAuth);
 
 app.listen(PORT, () => console.log("Server running on port " + PORT));
--- src/utils/serverError.ts
+++
@@ -1,6 +0,0 @@
-
-import { Response } from 'express';
-
-export const sendErrorResponse = (res: Response, message: string, status: number = 500): void => {
-  res.status(status).send(message);
-};
